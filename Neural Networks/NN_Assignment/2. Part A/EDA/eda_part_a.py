135ebafc0e3d08b65e460bf0c05b4def782eae2e
Kenneth Ng <31913638+Todayisagreatday@users.noreply.github.com>
2021-01-16 22:59:19 +0800

    Delete Neural Networks directory

--- Neural Networks/NN_Assignment/2. Part A/EDA/eda_part_a.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from pandas_profiling import ProfileReport
-
-data = pd.read_csv("C:/Users/delim/Desktop/NN_Assignment/Part A/ctg_data_cleaned.csv")
-profile = ProfileReport(data)
-profile.to_file("cardiotocography_report.html")
